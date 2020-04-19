0f6555edc60fcd9abfc8e26290d93c9e812eeafd
jedidiahmay <jedmay83@gmail.com>
2020-04-19 16:56:30 -0500

    Launcher icon update

--- app/src/main/java/com/jedmay/termscheduler/MainActivity.java
+++ app/src/main/java/com/jedmay/termscheduler/MainActivity.java
@@ -1,8 +1,5 @@
 package com.jedmay.termscheduler;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import DataProvider.SampleData;
 import Database.WGUTermRoomDatabase;
