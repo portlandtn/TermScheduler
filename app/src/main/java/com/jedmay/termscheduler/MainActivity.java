c74fdb8303a8ac566e588fb66fa80f751b288ae3
jedidiahmay <jedmay83@gmail.com>
2020-04-26 13:09:54 -0500

    Landscape layouts added

--- app/src/main/java/com/jedmay/termscheduler/MainActivity.java
+++ app/src/main/java/com/jedmay/termscheduler/MainActivity.java
@@ -2,6 +2,7 @@ package com.jedmay.termscheduler;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView inProgressAssessmentTextView, passedAssessmentTextView, failedAssessmentTextView;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
